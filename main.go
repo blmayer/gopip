8d5d59dc4a5307f4c0976e229a2282b0ebfa796c
blmayer <bleemayer@gmail.com>
2021-05-10 23:36:54 -0300

    Quitting server after first request

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 )
 
-var count = 0
+var (
+	count  = 0
+	ctx    context.Context
+	cancel context.CancelFunc
+)
 
 func main() {
+	ctx, cancel = context.WithCancel(context.Background())
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -16,14 +23,19 @@ func main() {
 
 	http.HandleFunc("/", install)
 
-	err := http.ListenAndServe(":" + port, nil)
-	if err != nil {
-		panic(err)
-	}
+	go func() {
+		err := http.ListenAndServe(":"+port, nil)
+		if err != http.ErrServerClosed {
+			println(err)
+		}
+	}()
+
+	<-ctx.Done() // wait for the signal to gracefully shutdown the server
 }
 
 func install(w http.ResponseWriter, r *http.Request) {
 	count++
 	out := fmt.Sprintln("request", count)
 	w.Write([]byte(out))
+	cancel()
 }
