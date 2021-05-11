471d55fe0eb70d6a6eb7ef88deb0505380fdfc22
blmayer <bleemayer@gmail.com>
2021-05-11 02:01:51 -0300

    Started pip installation and zipping

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"time"
+	"os/exec"
 )
 
 var (
-	count  = 0
 	ctx    context.Context
 	cancel context.CancelFunc
 )
@@ -31,7 +31,7 @@ func main() {
 	go func() {
 		err := http.ListenAndServe(":"+port, nil)
 		if err != http.ErrServerClosed {
-			println(err)
+			log.Println(err)
 		}
 	}()
 
@@ -41,14 +41,47 @@ func main() {
 	// waiting indefinitely for connections to return to idle and then shut down.
 	err := srv.Shutdown(context.Background())
 	if err != nil {
-		println(err)
+		log.Println(err)
 	}
 }
 
 func install(w http.ResponseWriter, r *http.Request) {
-	count++
-	out := fmt.Sprintln("request", count)
-	time.Sleep(5 * time.Second)
-	w.Write([]byte(out))
-	// cancel()
+	packName := r.URL.Path[1:]
+
+	path := "/tmp/" + packName
+	err := os.Mkdir(path, os.ModeDir)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("installing", packName, "into", path)
+	cmd := exec.Command("pip", "install", "--isolated", packName, "-t", path)
+
+	var pipOut bytes.Buffer
+	cmd.Stdout = &pipOut
+
+	err = cmd.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("zipping files")
+	cmd = exec.Command("zip", path+".zip", "-r", path)
+	var zipOut bytes.Buffer
+	cmd.Stdout = &zipOut
+
+	err = cmd.Run()
+	if err != nil {
+		log.Println(err)
+	}
+
+	// TODO: Clean pack files (to free memory)
+
+	// Read zip and save
+
+	// Upload zip or save to mounted volume
+
+	w.Write(append(pipOut.Bytes(), zipOut.Bytes()...))
+	cancel()
 }
